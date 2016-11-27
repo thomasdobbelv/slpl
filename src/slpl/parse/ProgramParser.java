f42dd88a396b4dbc65a0fbc2abeba8670320f5b7
Thomas Wingsternes <twing_87@hotmail.com>
2016-11-27 17:19:52 +0100

    dsa

--- src/slpl/parse/ProgramParser.java
+++ src/slpl/parse/ProgramParser.java
@@ -19,11 +19,14 @@ public class ProgramParser {
     }
 
     public static void main(String[] args) throws ParseException {
-//        String programText = "121 + 3/3 - 14 + (2*(2-1)/2) + 0.5 - 0 * 28";
+        String programText = "11.5 + 3/3 - 14 + (2*(2-1)/2) + 0.5 - 0 * 28 + 3*1.15";
 //        System.out.println(Lexer.lex(programText));
 //        System.out.println(ArithmeticExpressionParser.parseArithmeticExpression(0, Lexer.lex(programText)).fst.evaluate());
-        String programText = "2 - -1";
+//        String programText = "2 - -1";
         Ast program = parseProgram(programText);
+        System.out.println("Program: ");
+        System.out.println(program);
+        System.out.println("Program evaluated: ");
         System.out.println(program.evaluate());
     }
 
