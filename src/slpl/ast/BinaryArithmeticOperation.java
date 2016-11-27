c8c559b05360ceb5316d5c7dc195a4537f917924
Thomas Wingsternes <twing_87@hotmail.com>
2016-11-27 16:23:32 +0100

    renaming.

--- src/slpl/ast/BinaryArithmeticOperation.java
+++ src/slpl/ast/BinaryArithmeticOperation.java
@@ -3,29 +3,29 @@ package slpl.ast;
 public class BinaryArithmeticOperation extends Ast {
 
     private String operator;
-    private Ast arg0, arg1;
+    private Ast arg1, arg2;
 
-    public BinaryArithmeticOperation(String operator, Ast arg0, Ast arg1) {
+    public BinaryArithmeticOperation(String operator, Ast arg1, Ast arg2) {
         this.operator = operator;
-        this.arg0 = arg0;
         this.arg1 = arg1;
+        this.arg2 = arg2;
     }
 
     @Override
     public Ast evaluate() {
-        Number num0 = (Number) arg0.evaluate(), num1 = (Number) arg1.evaluate();
-        double val0 = num0.getValue(), val1 = num1.getValue();
+        Number num1 = (Number) arg1.evaluate(), num2 = (Number) arg2.evaluate();
+        double val1 = num1.getValue(), val2 = num2.getValue();
         switch (operator) {
-            case "+": return new Number((val0 + val1) + "");
-            case "-": return new Number((val0 - val1) + "");
-            case "*": return new Number((val0 * val1) + "");
-            case "/": return new Number((val0 / val1) + "");
+            case "+": return new Number((val1 + val2) + "");
+            case "-": return new Number((val1 - val2) + "");
+            case "*": return new Number((val1 * val2) + "");
+            case "/": return new Number((val1 / val2) + "");
         }
         throw new UnsupportedOperationException(operator);
     }
 
     @Override
     public String toString() {
-        return String.format("(BinaryArithmeticOperation %s %s %s)", operator, arg0, arg1);
+        return String.format("(BinaryArithmeticOperation %s %s %s)", operator, arg1, arg2);
     }
 }
