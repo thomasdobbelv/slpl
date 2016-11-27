993e754cc990c75498b9da422077696d3e0acc8c
Thomas Wingsternes <twing_87@hotmail.com>
2016-11-27 16:20:59 +0100

    added arity as a field to the operator enum type. added the unary minus operator (additive inverse). fromString now has arity as an additional parameter to help determine which operator to return

--- src/slpl/Operator.java
+++ src/slpl/Operator.java
@@ -2,17 +2,20 @@ package slpl;
 
 public enum Operator {
 
-    ADD("+", 0, Fixity.LEFT),
-    SUB("-", 0, Fixity.LEFT),
-    MUL("*", 1, Fixity.LEFT),
-    DIV("/", 1, Fixity.LEFT);
+    ADD("+", 2, 0, Fixity.LEFT),
+    SUB("-", 2, 0, Fixity.LEFT),
+    MUL("*", 2, 1, Fixity.LEFT),
+    DIV("/", 2, 1, Fixity.LEFT),
+    ADDITIVE_INVERSE("-", 1, 2, Fixity.RIGHT);
 
+    private final String operatorSymbol;
     private final Fixity fixity;
     private final int precedence;
-    private final String operatorSymbol;
+    private final int arity;
 
-    Operator(String operatorSymbol, int precedence, Fixity fixity) {
+    Operator(String operatorSymbol, int arity, int precedence, Fixity fixity) {
         this.operatorSymbol = operatorSymbol;
+        this.arity = arity;
         this.precedence = precedence;
         this.fixity = fixity;
     }
@@ -22,9 +25,9 @@ public enum Operator {
      * @param operatorSymbol
      * @return The {@link Operator} associated with <b>operatorSymbol</b>, or null if no such operator exists.
      */
-    public static Operator fromString(String operatorSymbol) {
+    public static Operator fromString(String operatorSymbol, int arity) {
         for(Operator o : values()) {
-            if(o.operatorSymbol.equals(operatorSymbol)) {
+            if(o.operatorSymbol.equals(operatorSymbol) && o.arity == arity) {
                 return o;
             }
         }
